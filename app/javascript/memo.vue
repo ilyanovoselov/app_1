2be6890002fa787ac7e1b62814a881525a0b1d24
= <app_1@194-67-116-37.cloudvps.regruhosting.ru>
2021-08-16 13:02:30 +0300

    Add Card component. v-for creates Card subcomponents

--- app/javascript/memo.vue
+++ app/javascript/memo.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app" v-if="seen">
     <p>{{ message }}</p>
-
-    <div v-for="img in images" class="card" :style="{ backgroundImage: `url(${img.src})` }" :alt="img.name">
-    </div>
+    <card v-for="img in images" v-bind:key="img.id" v-bind:card_data="img"></card>
   </div>
 </template>
 
@@ -15,7 +13,7 @@ export default {
       message: "Vue Memo Game!",
       seen: true,
       images: [
-          {name: 'eltex_1', id: 1, src: 'images/memo/1_eltex/ME5000.png'},
+          {name: 'eltex_1', id: 1, src: 'images/memo/1_eltex/olt-ma4000_px_l.png'},
       ]
     }
   },
@@ -26,11 +24,10 @@ export default {
     this.images =  data;
     console.log(data);
   }
-
 }
 </script>
 
-<style scoped>
+<style>
 #app{
   width:500px;
   margin-left: auto;
@@ -41,14 +38,43 @@ p {
   text-align: center;
 }
 
+
 .card{
   width:110px;
   height: 110px;
   margin:5px;
+  cursor: pointer;
   box-shadow: 0 0 5px 2px #cfcfcf;
   display: inline-block;
+  transition: all 0.5s;
+  transform-style: preserve-3d;
+  /* transform-origin: center right; */
+  position: relative;
+}
+
+.card-face{
+  width: 100%;
+  height: 100%;
   background-repeat: no-repeat;
   background-size: 100%;
   background-position: center;
+  display: block;
+
+  -webkit-backface-visibility: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.card-back{
+  transform: rotateY(180deg);
+  z-index: 2;
+  background-color:grey;
+}
+
+.card:hover{
+  /* transform:scale(1.02);
+  box-shadow: 0 0 6px 1px #cfcfcf; */
+  transform: rotateY(180deg);
 }
 </style>
--- app/javascript/packs/memo.js
+++ app/javascript/packs/memo.js
@@ -1,7 +1,25 @@
-import Vue from 'vue'
+// import Vue from 'vue'
+import Vue from 'vue/dist/vue.esm' //version with template compiler (for multicomponent template render usage)
+
 import App from '../memo.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  Vue.component('card', {
+    props: ['card_data'],
+    template:'<div class="card"><div  class="card-face card-front" :style="{ backgroundImage: `url(${card_data.src})` }" :alt="card_data.name"></div><div class="card-face card-back"></div></div>'
+
+    // template:`
+    //   <div class='card'>
+    //       <div class="card-face card-front" :style="{backgroundImage:'url(card_data.src)'}" :alt="card_data.name" :data-src="card_data.src">
+    //       </div>
+    //       <div  class="card-face card-back">
+    //       </div>
+    //   </div>
+    // `,
+
+  })
+
   const app = new Vue({
     render: h => h(App)
   }).$mount()

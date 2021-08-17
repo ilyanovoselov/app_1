81c1bfcc8f734572c19f5c5df9f9e0d3364cbd51
ilya <app_1@194-67-116-37.cloudvps.regruhosting.ru>
2021-08-17 06:46:27 +0300

    Add click event to card. Some css fixes. remove test rails image tag from template

--- app/javascript/card.vue
+++ app/javascript/card.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class='card'>
-    <div class="card-face card-front" :style="{ backgroundImage: `url(${card_data.src})` }" :alt="card_data.name" :data-src="card_data.src">
-    </div>
+  <div class='card' v-on:click="isActive = !isActive" v-bind:class="{ active: isActive }">
     <div  class="card-face card-back">
+    </div>
+    <div class="card-face card-front" :style="{ backgroundImage: `url(${card_data.src})` }" :alt="card_data.name">
     </div>
   </div>
 </template>
@@ -13,14 +13,62 @@ export default {
   props: ['card_data'],
   data: function () {
     return {
-      message: "Card message"
+      message: "Card message",
+      isActive: false
     }
   }
 }
 </script>
 
 <style scoped>
-.card-component {
-  background-color: cyan;
+.card{
+  width:110px;
+  height: 110px;
+  margin:5px;
+  cursor: pointer;
+  box-shadow: 0 0 5px 2px #cfcfcf;
+  display: inline-block;
+  transition: all 0.3s;
+
+  transition-timing-function:  cubic-bezier(.27,-0.34,.73,1.03);
+  transform-style: preserve-3d;
+  /* transform-origin: center right; */
+  position: relative;
+}
+
+.card-face{
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: center;
+  display: block;
+
+  -webkit-backface-visibility: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.card-front{
+  transform: rotateY(180deg);
+
+}
+.card-back{
+  z-index: 2;
+  background-image: url('/images/memo/memo_card_back.png');
+}
+
+.card:hover{
+  transform:scale(1.02);
+  box-shadow: 0 0 6px 1px #cfcfcf;
+}
+
+.card.active{
+  transform: rotateY(180deg);
+}
+
+.card.active:hover{
+  transform: rotateY(180deg) scale(1.02);
 }
 </style>
--- app/javascript/memo.vue
+++ app/javascript/memo.vue
@@ -39,44 +39,4 @@ p {
   font-size: 2em;
   text-align: center;
 }
-
-
-.card{
-  width:110px;
-  height: 110px;
-  margin:5px;
-  cursor: pointer;
-  box-shadow: 0 0 5px 2px #cfcfcf;
-  display: inline-block;
-  transition: all 0.3s;
-  transform-style: preserve-3d;
-  /* transform-origin: center right; */
-  position: relative;
-}
-
-.card-face{
-  width: 100%;
-  height: 100%;
-  background-repeat: no-repeat;
-  background-size: 100%;
-  background-position: center;
-  display: block;
-
-  -webkit-backface-visibility: hidden;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-
-.card-back{
-  transform: rotateY(180deg);
-  z-index: 2;
-  background-color:grey;
-}
-
-.card:hover{
-  transform:scale(1.02);
-  box-shadow: 0 0 6px 1px #cfcfcf;
-  /* transform: rotateY(180deg); */
-}
 </style>

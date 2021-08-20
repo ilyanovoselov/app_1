b63345225b0e9dc3f59bfea98bf70ba1d5070299
ilya <app_1@194-67-116-37.cloudvps.regruhosting.ru>
2021-08-20 20:19:44 +0300

    App sliced to main memo file and game desk

--- app/javascript/memo.vue
+++ app/javascript/memo.vue
@@ -1,156 +1,77 @@
 <template>
-  <div id="app" v-if="seen" v-bind:class="[activeClass]">
-    <button class="primary" v-on:click="setField(4)" >Поле 4</button>
-    <button class="primary" v-on:click="setField(16)" >Поле 16</button>
-    <button class="primary" v-on:click="setField(36)" >Поле 36</button>
-    <button class="primary" v-on:click="tossCards()" >Перетасовать</button>
-    <p>{{ message }}</p>
-    <card v-for="(img,index) in images" v-bind:key="img.id" v-bind:card_data="img" v-bind:custom_index="index" @select="rememberSelected" ref="cardChild"></card>
+  <div id="app" >
+    <div id="menu">
+      <button v-on:click="startGame()">Show desk</button>
+      <button v-on:click="showSettings()">Settings</button>
+    </div>
+
+    <div v-if="seenSettings" class="settings">
+        <ul>
+          <li>Настройка 1</li>
+          <li>Настройка 2</li>
+          <li>Настройка 3</li>
+          <li>Настройка 4</li>
+        </ul>
+    </div>
+    <desk v-bind:desk_data="seenDesk"></desk>
   </div>
 </template>
 
 <script>
 import Card from './card'
+import Desk from './desk'
 
 export default {
-  components: {Card},
+  components: {Card,Desk},
   data: function () {
     return {
-      message: "Vue Memo Game!",
-      seen: true,
-      images: [
-          {name: 'eltex_1', id: 1, src: 'images/memo/1_eltex/olt-ma4000_px_l.png',status:''},
-      ],
-      size:16,
-      activeClass: 'size_16',
-      score:0,
-      selected_cards:[]
+      message: "desk!",
+      seenMenu: true,
+      seenDesk: true,
+      seenSettings: false
     }
   },
   created(){
-    this.getCards(this.size);
+
   },
   methods: {
-    setField: function (newsize) {
-      this.size = newsize;
-      this.activeClass = 'size_'+this.size;
-      this.getCards(this.size);
+    startGame: function(){
+      this.seenSettings = false;
+      this.seenDesk = !this.seenDesk;
     },
-    async getCards() {
-      let url = 'http://novoselovilya.ru/memo/images?size='+this.size;
-      let response = await fetch(url);
-      let data = await response.json();
-      this.images = await data;
-      // console.log(data);
-      this.resetGame();
+    showSettings: function(){
+      this.seenDesk = false;
+      this.seenSettings = !this.seenSettings;
 
-      setTimeout(()=>{
-        this.tossCards();
-      },100);
-    },
-    tossCards: function(){
-      this.$refs.cardChild.forEach(async card_child => {
-        card_child.$el.style.transition = 'none';
-        let temp_x = this.screenCenter.x - card_child.card_coord.x;
-        let temp_y = this.screenCenter.y - card_child.card_coord.y;
-        await Velocity(card_child.$el, {translateX: temp_x+"px",translateY: temp_y+"px"});
-        await Velocity(card_child.$el, 'reverse');
-        card_child.$el.style='';
-      })
-    },
-    resetGame: function(){
-      this.score = 0;
-      this.selected_cards = [];
-      // this.message = "Game has restarted!";
-    },
-    rememberSelected: function(incoming_card_data){
-      // console.log(incoming_card_data);
-      //get card status not empty
-      if(this.selected_cards.length == 0){
-        //update 1st card
-        this.selected_cards[0] = this.images[incoming_card_data.card_id]; // remember 1st card
-        this.selected_cards[0].status = 'pending';
-
-      } else if(this.selected_cards.length == 1){
-
-        //check for click on current active card
-        if(this.selected_cards[0].card_id == incoming_card_data.card_id){
-          this.message = 'Please choose another card';
-          return false;
-        }
-        //update 2nd card
-        this.selected_cards[1] = this.images[incoming_card_data.card_id]; // remember 2d card
-        this.selected_cards[1].status = 'pending';
-
-        let temp_status = '';
-        let temp_message = '';
-        if(this.selected_cards[0].pair_id ==this.selected_cards[1].pair_id){
-          this.score +=2;
-          this.message = 'Yeap!';
-          temp_message = 'Your score is: '+this.score;
-          temp_status = 'pass';
-          if(this.score == this.size){
-            this.message = 'Congratulations!';
-          }
-        } else {
-          this.message = 'Wrong guess!';
-          temp_message ='Please select another pair';
-          temp_status = ''
-        }
-
-        setTimeout(()=>{
-          this.message = temp_message;
-          this.selected_cards[0].status = temp_status;
-          this.selected_cards[1].status = temp_status;
-          this.selected_cards=[];
-
-          if(this.score == this.size){ // show all cards after game is completed
-            Object.entries(this.images).forEach(([key, image_object]) => {
-              Object.keys(image_object).forEach(function(key){ image_object['status'] = "pending" });
-            });
-          }
-        }, 2000);
-
-      }
     }
   },
   computed:{
-    screenCenter: function(){
-      return {
-        x:window.innerWidth/2,
-        y:window.innerHeight/2
-      }
-    }
+
   }
 }
 </script>
 
 <style scoped>
-#app{
-  width:500px;
-  margin-left: auto;
-  margin-right: auto;;
-}
-#app.size_36{
-  width: 750px;
-  margin-left: auto;
-  margin-right: auto;;
-}
-p {
-  font-size: 2em;
-  text-align: center;
-}
+  #app{
+    width:500px;
+    margin-left: auto;
+    margin-right: auto;
+  }
 
-button.primary{
-  cursor:pointer;
-  border:none;
-  background-color: #72c3f7;
-  color:white;
-  padding:10px 20px;
-  border-radius: 1px;
-  transiton: all 0.2s ease-out;
-}
-button.primary:hover{
-  background-color: #5fa9d9;
-}
+  #menu button{
+    cursor:pointer;
+
+    width:100%;
+    text-align: center;
+    border:none;
+    background-color: #72c3f7;
+    padding:10px 30px;
+    font-weight: bold;
+    color:white;
+    margin:5px 0;
+  }
+
+  #menu button:hover{
+    background-color: #5fa9d9;
+  }
 </style>
